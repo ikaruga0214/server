9f50776afc26e7605b464a068fa452cd82a5982a
Steven <ikaruga0214@gmail.com>
2022-05-08 23:27:29 +0800

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: '556805317083-7n43s5fr32f7lv0oln92nm876rb9hv0k.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb+srv://ikaruga0214:atago114514@ayaya.ay1da.mongodb.net/AYAYA?retryWrites=true&w=majority',
+};
